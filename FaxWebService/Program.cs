e00c4b54a6f19f62882d6466f9d8672026c1e318
Johannes <johannes.haick@gmail.com>
2023-11-09 15:36:28 +0100

    add cors for frontend

--- FaxWebService/Program.cs
+++ FaxWebService/Program.cs
@@ -4,6 +4,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: FrontendSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:8081");
+                      });
+});
+
 Worker.Initiliaze();
 
 var app = builder.Build();
