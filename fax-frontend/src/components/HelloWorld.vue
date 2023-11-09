d6607ec1dcb01e74ac73b940415956f3f4a5bd10
Fittkau Luis <luis.fittkau@d-velop.de>
2023-11-09 16:01:36 +0100

    add basic frontend

--- fax-frontend/src/components/HelloWorld.vue
+++ fax-frontend/src/components/HelloWorld.vue
@@ -1,33 +1,64 @@
 <template>
     <div class="heading">StraightUpFax</div>
-    <div v-if="loading">Loading Fax...</div>
+    <div v-if="message">{{message}}</div>
     <div class="quote"
          v-for="fact in post"
          :key="fact">
-        <p>{{fact}}</p>
+        {{fact}}
+        <button @click="rmFact(fact)">Remove fact</button>
+    </div>
+    <div>
+        <input v-model="newFact" />
+        <button @click="addFact(newFact)">Add fact</button>
     </div>
 </template>
 
 <script setup>
     import { ref } from 'vue'
 
-    const loading = ref(true);
+    const message = ref("Loading Fax...");
     const post = ref(null)
+    const newFact = ref('')
+    const url = 'https://localhost:8080/' //https://192.168.188.162:8080/
 
     fetchFax()
 
     async function fetchFax() {
         post.value = null;
-        loading.value = true;
+        message.value = 'Loading Fax...'
+        try {
+            /*await fetch('http://192.168.188.162:8080/getAllFax')*/
+            await fetch(url + 'getAllFax')
+                .then(r => r.json())
+                .then(json => {
+                    post.value = json;
+                    message.value = null;
+                    return;
+                });
+        }
+        catch {
+            message.value = 'Fax server could not be reached.';
+        }
+    }
 
-        /*await fetch('http://192.168.188.162:8080/getAllFax')*/
-        await fetch('https://localhost:8080/getAllFax')
-            .then(r => r.json())
-            .then(json => {
-                post.value = json;
-                loading.value = false;
-                return;
-            });
+    async function rmFact(fact) {
+        try {
+            await fetch(url + 'removeFax', { method: 'POST', body: fact })
+        }
+        catch {
+            message.value = 'Error removing fact.';
+        }
+        await fetchFax()
+    }
+
+    async function addFact(fact) {
+        try {
+            await fetch(url + 'addFax', { method: 'POST', body: fact })
+        }
+        catch {
+            message.value = 'Error adding fact.';
+        }
+        await fetchFax()
     }
 </script>
 
@@ -45,5 +76,8 @@ li {
 }
 a {
   color: #42b983;
+}
+.heading {
+    border: 2px solid black;
 }
 </style>
